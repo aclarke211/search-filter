70ba8a804353f1305d486b6975ea6d37c7d9a8cc
Alex Clarke <Alex.Clarke@jdplc.com>
2019-04-29 17:35:07 +0100

    Setup original and filtered data to show on-screen.

--- src/components/FilterExample.vue
+++ src/components/FilterExample.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="filter__outer">
-    <h1>This is the Filter component.</h1>
+    <div class="default-data data">
+      <h2 class="default-data__title data-title">Default Data</h2>
+      <p class="default-data__json data-json">{{ this.items }}</p>
+    </div>
+
+    <div class="filters">
+      <h2 class="filters__title data-title">Filters</h2>
+    </div>
+
+    <div class="filtered-data data">
+      <h2 class="filter-data__title data-title">Filtered Data</h2>
+      <p class="filtered-data__json data-json">{{ this.filteredData }}</p>
+
+    </div>
   </div>
 </template>
 
@@ -32,5 +45,27 @@ export default {
       },
     ],
   }),
+
+  computed: {
+    filteredData() {
+      return this.items;
+    },
+  },
 };
 </script>
+
+<style lang="scss">
+  .data {
+    margin: 3rem 0;
+
+    &-title {
+      border-bottom: 2px solid slategrey;
+      display: inline-flex;
+      padding: 0 .25rem .25rem;
+    }
+
+    &-json {
+      font-size: .8rem;
+    }
+  }
+</style>
