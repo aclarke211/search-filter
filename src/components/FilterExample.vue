74febc6f9ec3f423473644fa71976a7a091ea42f
Alex Clarke <Alex.Clarke@jdplc.com>
2019-04-29 17:47:29 +0100

    Setup select boxes for filtering data.

--- src/components/FilterExample.vue
+++ src/components/FilterExample.vue
@@ -7,6 +7,27 @@
 
     <div class="filters">
       <h2 class="filters__title data-title">Filters</h2>
+      <div class="select-filters">
+        <div
+          v-for="(selectFilter, selectKey) in selectFilters"
+          :key="selectKey"
+          class="select__container">
+            <h3>{{ selectFilter.label }}</h3>
+
+            <select
+            class="select-box"
+            v-model="selectFilter.current">
+              <option
+                v-for="(singleOption, optionKey) in selectFilter.options"
+                :key="optionKey"
+                v-bind:value="singleOption.value"
+                :class="['select-option', `select-option-${optionKey}`]">
+                  {{ singleOption.text }}
+              </option>
+            </select>
+
+          </div>
+      </div>
     </div>
 
     <div class="filtered-data data">
@@ -44,6 +65,59 @@ export default {
         services: ['Email Builder'],
       },
     ],
+
+    selectFilters: [
+      {
+        label: 'Role',
+        current: '',
+        options: [
+          {
+            text: 'All',
+            value: '',
+          },
+          {
+            text: 'Admin',
+            value: 'admin',
+          },
+        ],
+      },
+      {
+        label: 'Fascia',
+        current: '',
+        options: [
+          {
+            text: 'All',
+            value: '',
+          },
+          {
+            text: 'JD Sports (GB)',
+            value: 'JDSPORTS',
+          },
+          {
+            text: 'JD Sports (BE)',
+            value: 'JDSPORTS_BE',
+          },
+        ],
+      },
+      {
+        label: 'Services',
+        current: '',
+        options: [
+          {
+            text: 'All',
+            value: '',
+          },
+          {
+            text: 'Content Editor',
+            value: 'content_editor',
+          },
+          {
+            text: 'Email Builder',
+            value: 'email_builder',
+          },
+        ],
+      },
+    ],
   }),
 
   computed: {
@@ -66,6 +140,23 @@ export default {
 
     &-json {
       font-size: .8rem;
+    }
+  }
+
+  .filters {
+    .select-filters {
+      display: flex;
+
+      .select__container {
+        margin: 1rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .select-box {
+          margin: .5rem;
+        }
+      }
     }
   }
 </style>
