cb850f1e46c5b070c0e591233792d99c05114fc6
Alex Clarke <Alex.Clarke@jdplc.com>
2019-04-29 17:25:17 +0100

    Add default items to filer.

--- src/components/FilterExample.vue
+++ src/components/FilterExample.vue
@@ -7,5 +7,30 @@
 <script>
 export default {
   name: 'FilterExample',
+
+  data: () => ({
+    items: [
+      {
+        role: 'Admin',
+        fascia: 'JDSPORTS',
+        services: ['Content Editor'],
+      },
+      {
+        role: 'User',
+        fascia: 'JDSPORTS',
+        services: ['Content Editor'],
+      },
+      {
+        role: 'Admin',
+        fascia: 'JDSPORTS_BE',
+        services: ['Content Editor'],
+      },
+      {
+        role: 'User',
+        fascia: 'JDSPORTS',
+        services: ['Email Builder'],
+      },
+    ],
+  }),
 };
 </script>
