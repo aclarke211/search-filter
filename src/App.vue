51fa47f41cad7be770f37f497e39e631c6d32aa4
Alex Clarke <Alex.Clarke@jdplc.com>
2019-04-29 17:22:24 +0100

    Setup FilterExample component.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title">Filter / Search</h1>
+    <FilterExample />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import FilterExample from '@/components/FilterExample.vue';
 
 export default {
   name: 'app',
+
   components: {
-    HelloWorld,
+    FilterExample,
   },
 };
 </script>
---
+++ src/components/FilterExample.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="filter__outer">
+    <h1>This is the Filter component.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilterExample',
+};
+</script>
